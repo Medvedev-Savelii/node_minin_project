208335163c45a6268f4b13920c5458acdec23f2d
sava <savamedvedev@gmail.com>
2022-07-08 19:30:58 +0800

    second-commit

--- server/app.js
+++ server/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
+const chalk = require("chalk");
 
 const PORT = config.get("port") ?? 5000;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+if (process.env.NODE_ENV === "production") {
+  console.log(chalk.bgGreen("Production"));
+} else {
+  console.log(chalk.bgYellow("Development"));
+}
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get("mongoUri"));
+    console.log(chalk.bgBlue("Connect DB"));
+    app.listen(PORT, () => {
+      console.log(chalk.bgBlue(`Server started on port ${PORT}`));
+    });
+  } catch (error) {
+    console.log(chalk.bgRgb(error.message));
+  }
+};
+
+start();
