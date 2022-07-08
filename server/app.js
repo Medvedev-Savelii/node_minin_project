1d1638e978ed1e34881e96228b6d31fe5276b96a
sava <savamedvedev@gmail.com>
2022-07-08 22:14:54 +0800

    five-commit

--- server/app.js
+++ server/app.js
@@ -3,12 +3,17 @@ const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
 const initDatabase = require("./startUp/initDatabase");
+const routes = require("./routes");
 
 const PORT = config.get("port") ?? 5000;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api", routes);
+
+
+
 
 if (process.env.NODE_ENV === "production") {
   console.log(chalk.bgGreen("Production"));
