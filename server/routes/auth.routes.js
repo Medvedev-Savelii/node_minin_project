d4ef68fc995459812e631824aa431ad2f84d1d07
sava <savamedvedev@gmail.com>
2022-07-09 22:01:30 +0800

    seven-commit

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,43 @@
 const express = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const { generateUserData } = require("../utils/helpers");
+const tokenService = require("../services/token.service");
 const router = express.Router({ mergeParams: true });
 
 router.post("/singUp", async (req, res) => {
   try {
-  } catch (error) {}
+    const { email, password } = req.body;
+
+    const exitingUser = await User.findOne({ email });
+
+    if (exitingUser) {
+      return res.status(400).json({
+        error: {
+          message: "EMAIL_EXISTS",
+          code: 400,
+        },
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = await User.create({
+      ...generateUserData(),
+      ...req.body,
+      password: hashedPassword,
+    });
+
+    const tokens = tokenService.generate({ _id: newUser._id });
+    await tokenService.save(newUser._id, tokens.refreshToken);
+
+    res.status(201).send({ ...tokens, userId: newUser._id });
+  } catch (error) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже",
+    });
+  }
 });
 
 router.post("/singInWithPassword", async (req, res) => {
