9457f5d375eadcb7a5d986eaf915a0a68af8f785
sava <savamedvedev@gmail.com>
2022-07-09 23:24:10 +0800

    night-commit

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,44 +6,108 @@ const { generateUserData } = require("../utils/helpers");
 const tokenService = require("../services/token.service");
 const router = express.Router({ mergeParams: true });
 
-router.post("/singUp", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post("/singUp", [
+  check("email", "Некорректный email").isEmail(),
+  check("password", "Минимальная длина пароля 8 символов").isLength({ min: 8 }),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: {
+            message: "INVALID_DATA",
+            code: 400,
+            errors: errors.array(),
+          },
+        });
+      }
 
-    const exitingUser = await User.findOne({ email });
+      const { email, password } = req.body;
 
-    if (exitingUser) {
-      return res.status(400).json({
-        error: {
-          message: "EMAIL_EXISTS",
-          code: 400,
-        },
+      const exitingUser = await User.findOne({ email });
+
+      if (exitingUser) {
+        return res.status(400).json({
+          error: {
+            message: "EMAIL_EXISTS",
+            code: 400,
+          },
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const newUser = await User.create({
+        ...generateUserData(),
+        ...req.body,
+        password: hashedPassword,
+      });
+
+      const tokens = tokenService.generate({ _id: newUser._id });
+      await tokenService.save(newUser._id, tokens.refreshToken);
+
+      res.status(201).send({ ...tokens, userId: newUser._id });
+    } catch (error) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка. Попробуйте позже",
       });
     }
+  },
+]);
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+router.post("/singInWithPassword", [
+  check("email", "Email некорректный").normalizeEmail().isEmail(),
+  check("password", "Пароль не может быть пустым").exists(),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: {
+            message: "INVALID_DATA",
+            code: 400,
+          },
+        });
+      }
 
-    const newUser = await User.create({
-      ...generateUserData(),
-      ...req.body,
-      password: hashedPassword,
-    });
+      const { email, password } = req.body;
 
-    const tokens = tokenService.generate({ _id: newUser._id });
-    await tokenService.save(newUser._id, tokens.refreshToken);
+      const existingUser = await User.findOne({ email });
 
-    res.status(201).send({ ...tokens, userId: newUser._id });
-  } catch (error) {
-    res.status(500).json({
-      message: "На сервере произошла ошибка. Попробуйте позже",
-    });
-  }
-});
+      if (!existingUser) {
+        return res.status(400).send({
+          error: {
+            message: "EMAIL_NOT_FOUND",
+            code: 400,
+          },
+        });
+      }
 
-router.post("/singInWithPassword", async (req, res) => {
-  try {
-  } catch (error) {}
-});
+      const isPasswordEqual = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+
+      if (!isPasswordEqual) {
+        return res.status(400).send({
+          error: {
+            message: "INVALID_PASSWORD",
+            code: 400,
+          },
+        });
+      }
+      const tokens = tokenService.generate({ _id: existingUser._id });
+      await tokenService.save(existingUser._id, tokens.refreshToken);
+
+      res.status(200).send({ ...tokens, userId: existingUser._id });
+	  
+    } catch (error) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка. Попробуйте позже",
+      });
+    }
+  },
+]);
 
 router.post("/token", async (req, res) => {
   try {
